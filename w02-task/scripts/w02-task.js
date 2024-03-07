8466ea74356d8abcc24bda626fe1d571d752871a
Cipher715 <atsushi.karino@gmail.com>
2024-03-07 15:29:04 +0900

    Modification

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -13,7 +13,7 @@ let profilePicture = "images/IMG_5416.png";
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('picture');
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
